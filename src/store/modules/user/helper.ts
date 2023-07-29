c62289579cb5597b989892543c68874f243dd7b8
火红色祥云 <809880192@qq.com>
2023-07-29 16:14:31 +0800

    change port and auther

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 火红色祥云 809880192@qq.com
+ * @Date: 2023-07-29 16:03:44
+ * @LastEditors: 火红色祥云 809880192@qq.com
+ * @LastEditTime: 2023-07-29 16:07:31
+ * @FilePath: \OpenAI_ChatGPT3.5_Web\src\store\modules\user\helper.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { ss } from '@/utils/storage'
 
 const LOCAL_NAME = 'userStorage'
@@ -16,7 +24,7 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
+      name: 'hhsxy',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
